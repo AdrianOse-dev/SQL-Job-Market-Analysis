2cebf7fccc9581a67c5f96d97b7bffe644140876
Oseremen Adrian Akhimien Williams <uyi286@usask.ca>
2026-09-23 09:18:43 -0600

    Initial SQL job market analysis project

---
+++ UPDATE job_applied.sql
@@ -0,0 +1,22 @@
+UPDATE job_applied
+SET contact = 'Adrian Williams'
+WHERE job_id = 1;
+
+UPDATE job_applied
+SET contact = 'Mandy Jones'
+WHERE job_id = 2;
+
+UPDATE job_applied
+SET contact = 'Jacob Chugtai'
+WHERE job_id = 3;
+
+UPDATE job_applied
+SET contact = 'Jian Yang'
+WHERE job_id = 4;
+
+UPDATE job_applied
+SET contact = 'Phillip Mokai'
+WHERE job_id = 5;
+
+SELECT *
+FROM job_applied;
